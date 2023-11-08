9cf280fc163fe53534f0810608edf28f14c18d7f
dttrue <danieltorres@pursuit.org>
2023-11-07 20:17:32 -0500

    daniels third commit

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# Electronics-inventory-web-app
+# Electronics Inventory Web Application
+
+
+This is a simple web application for managing electronics
+
+## Features
+
+- View a list of electronic resources displayed as stylis item-cards.
+- Remove resources from the list.
+- Update the "in stock" status for each resource.
+- Add new resources to the inventory.
+- Increment and decrement item counts.
+- Alert features for user interactions.
+- Responsive design for various screen sizes.
+- Attractive transitions for various item card images.
+- A footer with contact information.
+
+## Getting Started
+
+Follow these steps to get the web application up and running on your local machine!
+
+1. **Clone the Repository:**
+
+   ```Terminal
+   git@github.com:dttrue/Electronics-inventory-web-app.git
+
+   cd electronics-inventory-app 
+   ```
+## Usage
+
+- When you open the web application, you'll see a list of electornic recources displayed as stlish item-cards.
+- Each item-card includes the resource name, price, stock count, description, and attractive item images.
+- Use the "Remove" button to remove a resource from the list.
+- To update the "on stock" status, simply click on the stauts, and it will toggle between "in stock" and "out of stock."
+- You can increment and decrement item counts using the provided options.
+- Use the form to add new resources to the inventory. Ensure that all required fields are filled before submitting the form.
+- The web application includes alert features for user interactions to provide feedback on actions
+
+## Stylish Item Cards
+
+- The web application displays resources using stylish item cards. Each item card includes a resource name, price, stock counr, desrciption, and an attractive image. The CSS styles for the item cards are defined in the `styles.css` file. You can customize these styles to create a unique and visually applealing design.
+
+## Increment and Decrement Options
+
+- The item cards provide options to increment and decrement item counts. Click on the "➡️" button to increase the count and the "⬅️" button to decrease it. These options make it easy to manage the quantity of these options by modifying the JavaScript code in the `app.js`file.
+
+## Alert Features 
+
+- This web application includes alert features to provide feedback to users during interactions. For example, you'll receive an alert if you attempt to submit the form without filling in the required fields. THe alertd ar designed to rnchance the user experience and provide guidance on how to use the application effectively.
+
+## Transitions for Item Cards Images
+
+- The item card images are designed with smooth hover transitions to provide an attractive user experience. To costomize these transitions, naviagte to the `styles.css` file and look for the CSS rules that apply to the `.item-card img` and `item-card img:hover` selectors. You can adjust properties like`transform` to control the image behavior on hover.
+
+## Footer 
+
+- The web application includes a footer that appears at the bottom of the page. It contains information and copyright details. You can customize the footer's appearance by modifying the CSS rules for the `footer` and `.footer-content` classes in the `styles.css` file.
+
+# Acknowledgments
+
+- Special thanks to Steven Zhang and Maliq Taylor for contributions and support.
--- app.js
+++ app.js
@@ -1,1 +1,160 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const inventory = [];
 
+  function createInventoryItemCard(item) {
+    const itemCard = document.createElement("div");
+    itemCard.classList.add("item-card");
+
+    const itemImage = document.createElement("img");
+    itemImage.setAttribute("src", item.img);
+
+    const itemName = document.createElement("strong");
+    itemName.textContent = item.name;
+
+    const itemText = document.createElement("p");
+    itemText.textContent = item.description;
+
+    const itemPrice = document.createElement("p");
+    itemPrice.textContent = `Price: $${item.price}`;
+
+    const itemStock = document.createElement("p");
+    itemStock.textContent = `In Stock: ${item.stock}`;
+
+    const orderSelector = document.createElement("div");
+    orderSelector.classList.add("order-selector");
+
+    const orderCount = document.createElement("span");
+    orderCount.textContent = item.orderCount;
+
+    const incrementButton = document.createElement("span");
+    incrementButton.textContent = "➡️";
+
+    const decrementButton = document.createElement("span");
+    decrementButton.textContent = "⬅️";
+
+    incrementButton.addEventListener("click", () => {
+      orderCount.textContent = ++item.orderCount;
+    });
+
+    decrementButton.addEventListener("click", () => {
+      if (item.orderCount > 0) {
+        orderCount.textContent = --item.orderCount;
+      }
+    });
+
+    orderSelector.appendChild(decrementButton);
+    orderSelector.appendChild(orderCount);
+    orderSelector.appendChild(incrementButton);
+
+    itemCard.appendChild(itemImage);
+    itemCard.appendChild(itemName);
+    itemCard.appendChild(itemPrice);
+    itemCard.appendChild(itemStock);
+    itemCard.appendChild(itemText);
+    itemCard.appendChild(orderSelector);
+    
+
+    document.querySelector(".supplies").appendChild(itemCard);
+  }
+
+  function displayInventory() {
+    for (const item of inventory) {
+      createInventoryItemCard(item);
+    }
+  }
+
+  function orderItem() {
+    const orderedItems = inventory.filter((item) => item.orderCount > 0);
+    if (orderedItems.length === 0) {
+      alert("You have no items in your order.");
+    } else {
+      const message = orderedItems.map(
+        (item) =>
+          `${item.orderCount} order${item.orderCount > 1 ? "s" : ""} of ${
+            item.name
+          }`
+      );
+      alert(message.join("\n"));
+    }
+  }
+
+  document.querySelector("#supply-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const newitem = {
+      name: event.target.name.value,
+      price: parseFloat(event.target.price.value),
+      stock: parseInt(event.target.stock.value),
+      img: event.target.img.value,
+      orderCount: 0,
+    };
+
+    inventory.push(newitem);
+    createInventoryItemCard(newitem);
+
+    event.target.reset();
+  });
+
+  document.querySelector("#submit-button").addEventListener("click", orderItem);
+
+  const inventoryItems = [
+    {
+      name: "Beats Fit Pro True Wireless Earbuds",
+      price: 99.99,
+      stock: 50,
+      img: "imgs/ear_buds.jpeg",
+      description: "Lorem ipsum dolor sit amet ",
+      orderCount: 0,
+    },
+    {
+      name: `LG, UHD 4K TV, 65 inch UR80 series`,
+      price: 599.99,
+      stock: 50,
+      img: "imgs/tv.jpeg",
+      description: "Lorem ipsum dolor sit amet ",
+      orderCount: 0,
+    },
+    {
+      name: ` Xbox Series X  Console  1TB SSD`,
+      price: 349.99,
+      stock: 50,
+      img: "imgs/xbox.jpeg",
+      description: "Lorem ipsum dolor sit amet ",
+      orderCount: 0,
+    },
+    {
+      name: `iPhone 15 Pro Max, 128 GB Pink Unlocked `,
+      price: 799.99,
+      stock: 50,
+      img: "imgs/iphone.jpeg",
+      description: "Lorem ipsum dolor sit amet sit amet, ipsum dolor sit  ",
+      orderCount: 0,
+    },
+    {
+      name: `Sony PlayStation 5 Digital Edition`,
+      price: 399.99,
+      stock: 50,
+      img: "imgs/ps5.jpeg",
+      description: "Lorem ipsum dolor amet ",
+      orderCount: 0,
+    },
+    {
+      name: ` Amazon Fire TV Stick 4K with, Voice Remote`,
+      price: 29.99,
+      stock: 50,
+      img: "imgs/fire stick.jpeg",
+      description: "Lorem ipsum dolor sit amet sit amet ipsu, dol.",
+      orderCount: 0,
+    },
+   
+
+  
+
+  ];
+
+  for (const item of inventoryItems) {
+    inventory.push(item);
+  }
+
+  displayInventory();
+});
